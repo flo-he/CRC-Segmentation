ca89e486b825c9599f8b5c34783acc9686d689cb
flo-he <florian-he@gmx.de>
2019-09-10 22:32:10 +0200

    Added Train/Test split

--- convert_dataset.py
+++ convert_dataset.py
@@ -6,8 +6,15 @@ import glob
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 import argparse
+import shutil
 
-def drop_image(x, drop_threshold):
+def drop_empty_image(x, drop_threshold):
+    '''
+    Removes image and mask pair from consideration if it contains not enought information. \n
+    Args: \n
+        :x: mask    \n
+        :drop_threshold: minimum relative amount of nonzero pixels 
+    '''
     # count nonzero label
     nonzero_pixel = np.count_nonzero(x)
     rel_amount = nonzero_pixel / 250000    # images are 500x500
@@ -33,8 +40,8 @@ def check_for_valid_seg_map(image, mask, drop_threshold=0.15):
     img_gray = np.abs(img_gray - 255.)
 
     # plotting just for checking by eye
-    plt.subplot(131)
-    plt.imshow(img_gray / 255, cmap="gray")
+    #plt.subplot(131)
+    #plt.imshow(img_gray / 255, cmap="gray")
 
     # smooth the image, so that small black gaps in tissue do not raise false alarm
     # (masks do not these little gaps and treat them as tissue anyway)
@@ -42,11 +49,11 @@ def check_for_valid_seg_map(image, mask, drop_threshold=0.15):
     img_gray = img_gray / np.max(img_gray)
 
     # plotting just for checking by eye
-    plt.subplot(132)
-    plt.imshow(img_gray, cmap='gray')
-    plt.subplot(133)
-    plt.imshow(mask, cmap="gray")
-    plt.show()
+    #plt.subplot(132)
+    #plt.imshow(img_gray, cmap='gray')
+    #plt.subplot(133)
+    #plt.imshow(mask, cmap="gray")
+    #plt.show()
 
     # which pixel are non background according to the mask?
     pixel_mask = np.logical_or(mask == 1, mask == 2)
@@ -64,8 +71,8 @@ def check_for_valid_seg_map(image, mask, drop_threshold=0.15):
     # relative amount of pixels where the mask overlaps with the image
     rel_amount = n_empty / np.count_nonzero(pixel_mask)
     # for checking
-    print(n_empty, np.count_nonzero(pixel_mask))
-    print(rel_amount)  
+    #print(n_empty, np.count_nonzero(pixel_mask))
+    #print(rel_amount)  
 
     # image is valid if the amount of empty pixels is not too high
     if rel_amount < drop_threshold:
@@ -73,7 +80,7 @@ def check_for_valid_seg_map(image, mask, drop_threshold=0.15):
     else:
         return False
 
-def convert_dataset(masks_fnames, output_masks_dir, output_frames_dir, drop_threshold=0.):
+def convert_dataset(masks_fnames, output_dir, drop_threshold=0.):
     '''
     Args: \n
         :mask_fnames: list of mask locations. \n
@@ -101,7 +108,7 @@ def convert_dataset(masks_fnames, output_masks_dir, output_frames_dir, drop_thre
 
         # if the mask is almost empty (i.e. the image is black) drop the image from the dataset
         if drop_threshold > 0.:
-            if drop_image(mask_arr, drop_threshold):
+            if drop_empty_image(mask_arr, drop_threshold):
                 continue
             else:
                 # read in corresponding image
@@ -113,7 +120,7 @@ def convert_dataset(masks_fnames, output_masks_dir, output_frames_dir, drop_thre
                 # check for validity
                 valid = check_for_valid_seg_map(img_arr, mask_arr)
                 # print for info
-                print(valid)
+                #print(valid)
                 if not valid:
                     continue
                 else:
@@ -121,38 +128,127 @@ def convert_dataset(masks_fnames, output_masks_dir, output_frames_dir, drop_thre
                     new_filename = file.split('\\')[-1].replace(".png", ".npz")
 
                     # save as npz
-                    np.savez_compressed(os.path.join(output_masks_dir, new_filename), mask_arr)
+                    np.savez_compressed(os.path.join(output_dir, new_filename), mask_arr)
             
                     # convert frame 
                     new_filename = file_frame.split('\\')[-1].replace(".png", ".npz")
-                    np.savez_compressed(os.path.join(output_frames_dir, new_filename), img_arr)
+                    np.savez_compressed(os.path.join(output_dir, new_filename), img_arr)
 
+def get_train_test_split(files, test_proportion=0.20, shuffle=True):
+    '''
+    Args:   \n
+        :masks: array of mask files  \n
+        :test_proportion: percentage of instances used for testing  \n
+        :shuffle: shuffle dataset before splitting
+    '''
+
+    n_instances = len(files)
+
+    if shuffle:
+        files = np.random.permutation(files)
+
+    n_test = int(n_instances*test_proportion)
+
+    test_files = files[:n_test].copy()
+    train_files = files[n_test:].copy()
+
+    return train_files, test_files
+
+def run_multithreaded_conversion(files, output_dir, n_jobs, drop_threshold=0.01):
+    '''
+    Apply convert_dataset via multiprocessing.  \n
+    
+    Args: \n
+        :files: files to prepare    \n
+        :output_dir: directory to which the .npz files are written  \n
+        :n_jobs: number of threads to spawn \n
+        :drop_threshold: see doc of convert_dataset
+    '''
+
+    # split for multiprocessing
+    mask_splits = np.array_split(files, n_jobs)
+
+    # execute the conversion
+    processes = []
+    for split in mask_splits:
+        p = mp.Process(target=convert_dataset, args=(split, output_dir, drop_threshold))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+def move_files(files, to_dir):
+    '''
+    Args:
+        :files: files to move
+        :to_dir: directory to move the files to
+    '''
+
+    for file in files:
+        shutil.move(file, os.path.join(to_dir, file.split("\\")[-1]))
+
+def run_multithreaded_move(files, to_dir, n_jobs):
+    '''
+    Args:   \n
+        :files: files to move   \n
+        :to_dir: directory to move the files to \n
+        :n_jobs: number of threads to spawn
+    '''
+    # split for mp
+    splits = np.array_split(files, n_jobs)
+
+    # run mp
+    processes = []
+    for split in splits:
+        p = mp.Process(target=move_files, args=(split, to_dir))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
 
 # Parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--root_dir', type=str, metavar='', required=True, help='Directory to the CRC dataset.')
 parser.add_argument('-t', '--threads', type=int, metavar='', help='Number of threads to spawn. Default: all available Cores.')
+parser.add_argument('-s', '--test_split', type=float, metavar='', help='rel. amount of test instances to split from all data.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    
-    # input frames
-    masks = glob.glob(os.path.join(args.root_dir, "masks\\*.png"))
+    # seed for split between training and test set
+    np.random.seed(42)
+
+    # get current working directory
+    cwd = os.getcwd()
+
+    # input masks
+    masks = np.asarray(glob.glob(os.path.join(args.root_dir, "masks\\*.png")))
+
     # for checking random check valid func
-    masks = np.asarray(masks)[np.random.randint(low=0, high=len(masks), size=30)]
+    #masks = np.asarray(masks)[np.random.randint(low=0, high=len(masks), size=30)]
 
     # output directories
-    output_frames = os.path.join(os.getcwd(), "data\\frames")
-    output_masks = os.path.join(os.getcwd(), "data\\masks")
+    output_dir = os.path.join(cwd, "data\\temp")
 
-    print(f"Write images .npz files to {output_frames}")
-    print(f"Write masks .npz files to {output_masks}")
+    output_tr_frames = os.path.join(cwd, "data\\train\\frames")
+    output_tr_masks = os.path.join(cwd, "data\\train\\masks")
 
+    output_ts_frames = os.path.join(os.getcwd(), "data\\test\\frames")
+    output_ts_masks = os.path.join(os.getcwd(), "data\\test\\masks")
+
+    print(f"Write train images .npz files to {output_tr_frames}")
+    print(f"Write train masks .npz files to {output_tr_masks}")
+    print(f"Write test images .npz files to {output_ts_frames}")
+    print(f"Write test masks .npz files to {output_ts_masks}")
     # create directory if not present
     try: 
         print("Trying to create data directories:")
-        os.makedirs(output_frames)
-        os.makedirs(output_masks)
+        os.makedirs(output_dir)
+        os.makedirs(output_tr_frames)
+        os.makedirs(output_tr_masks)
+        os.makedirs(output_ts_frames)
+        os.makedirs(output_ts_masks)
         print("Finished.")
     except FileExistsError:
         print("Some of the directories already exist.")
@@ -164,17 +260,37 @@ if __name__ == "__main__":
         num_cores = mp.cpu_count()
     print(f"Number of available CPU Cores: {num_cores}")
 
-    # split for multiprocessing
-    mask_splits = np.array_split(masks, num_cores)
-
-    # execute the conversion
-    processes = []
-    for split in mask_splits:
-        p = mp.Process(target=convert_dataset, args=(split, output_masks, output_frames, 0.01))
-        processes.append(p)
-        p.start()
-
-    for p in processes:
-        p.join()
+    # convert dataset
+    try:
+        run_multithreaded_conversion(masks, output_dir, num_cores)
+        print("Finished conversion of data.")
+    except:
+        raise RuntimeError("Conversion of data failed.")
     
-    print("Finished converting the Dataset.")
+    #############################
+
+    chosen_masks = glob.glob(os.path.join(output_dir, "mask*.npz"))
+
+    # get split
+    if args.test_split:
+        train_masks, test_masks = get_train_test_split(chosen_masks, args.test_split)
+        train_images, test_images = [name.replace("mask", "frame") for name in train_masks], [name.replace("mask", "frame") for name in test_masks]
+    else:
+        train_masks, test_masks = get_train_test_split(chosen_masks)
+        train_images, test_images = [name.replace("mask", "frame") for name in train_masks], [name.replace("mask", "frame") for name in test_masks]
+
+    # split into train and test set
+    try:
+        run_multithreaded_move(train_masks, output_tr_masks, num_cores)
+        run_multithreaded_move(train_images, output_tr_frames, num_cores)
+        run_multithreaded_move(test_masks, output_ts_masks, num_cores)
+        run_multithreaded_move(test_images, output_ts_frames, num_cores)
+        print("Finished splitting data.")
+    except:
+        raise RuntimeError("Splitting data failed.")
+    
+    try:
+        os.rmdir(output_dir)
+        print("Removed temporary dirs.")
+    except:
+        raise RuntimeError("Failed to remove temporary dirs.")
