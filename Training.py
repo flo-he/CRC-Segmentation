828d862787c12eadf2eccbb1421ad3f4f9f4fc6d
flo-he <florian-he@gmx.de>
2019-09-24 17:00:44 +0200

    Adapted training parameters

--- Training.py
+++ Training.py
@@ -2,7 +2,7 @@ import os
 
 import torch
 import torch.optim as optim
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Normalize
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from utils import MirrorPad
@@ -13,36 +13,38 @@ from multiprocessing import cpu_count
 from utils import complex_net, Dice_Loss, Dice_and_CE
 
 import logging
+from U_Net import NeuralNet
 
 
 # GLOBAL TRAINING PARAMETERS
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.backends.cudnn.benchmark = torch.cuda.is_available()
 
 train_dict= {
     "device" : device,
-    "epochs" : 100,
-    "batch_size" : 32,
-    "cv_folds": 3,
-    "images_per_epoch" : 1000,
+    "epochs" : 250,
+    "batch_size" : 1,
+    "cv_folds": 5,
+    "images_per_epoch" : 300,
     "pin_mem" : torch.cuda.is_available(),
     "num_workers" : 2,
-    "output_dir" : "models\\",
-    "train_from_chkpts" : [],#["C:\AML_seg_proj\CRC-Segmentation\models\model_chkpt_25.pt", "C:\AML_seg_proj\CRC-Segmentation\models\optimizer_chkpt_25.pt"],
-    "log_level" : logging.DEBUG
+    "output_dir" : "model_new\\",
+    "train_from_chkpts" : [],
+    "log_level" : logging.INFO
 }
 
 def main():
     # create pytorch dataset
     dataset_tr = CRC_Dataset(
         root_dir = os.path.join(os.getcwd(), "data\\train"),
-        transforms = [MirrorPad(((6,), (6,), (0,))), ToTensor()]
+        transforms = [MirrorPad(((6,), (6,), (0,))), ToTensor(), Normalize(mean=(0.7979, 0.6772, 0.7768), std=(0.1997, 0.3007, 0.2039), inplace=True)]
     )
 
     # set model, optimizer and loss criterion
-    model = complex_net()
+    model = NeuralNet(64, 128, 256, 512, 1024, droprate=0.5)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     criterion = Dice_and_CE(device).to(device)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5, min_lr=1e-6, verbose=True)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.2, patience=15, min_lr=1e-6, verbose=True)
 
     # initialize trainer class
     trainer = Trainer(model, optimizer, criterion, lr_scheduler, dataset_tr, **train_dict)
