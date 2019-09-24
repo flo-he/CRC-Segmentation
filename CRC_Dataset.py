36500405d9df7c480caae78cb41fead22d214c03
flo-he <florian-he@gmx.de>
2019-09-24 16:50:41 +0200

    added comp. of mean and std of datafor inp. norm.

--- CRC_Dataset.py
+++ CRC_Dataset.py
@@ -3,11 +3,10 @@ from torchvision.transforms import ToTensor, Compose
 import numpy as np
 import glob
 import os
-import time 
-from utils import MirrorPad
-import matplotlib.pyplot as plt
+#import time 
+#from utils import MirrorPad
+#import matplotlib.pyplot as plt
     
-
 class CRC_Dataset(torch.utils.data.Dataset):
     '''
     CRC Tissue Segmentation Dataset. Files are assumed to lie in a root directory with subfolders 'frames' and 'masks' with the compressed .npz file format.
@@ -47,26 +46,60 @@ class CRC_Dataset(torch.utils.data.Dataset):
 
         return image, torch.from_numpy(label).long()
 
+def compute_mean_and_std(dataloader, device):
+    '''
+    Compute the mean and standard deviation per channel of the whole dataset using first and second moments.
+    '''
 
+    # running means of first and sec. moment
+    mean = torch.zeros(3).to(device)
+    mean_sq = torch.zeros(3).to(device)
+
+    # loop over training set in batches, compute pixel means and batch means 
+    for img_batch, _ in dataloader:
+        n_img, channel = img_batch.size(0), img_batch.size(1)
+        img_batch = img_batch.view(n_img, channel, -1).to(device)
+
+        mean += torch.mean(img_batch, (0, 2))
+        mean_sq += torch.mean(img_batch**2, (0, 2))
+
+    # divide by # of batches 
+    mean /= len(dataloader)
+    mean_sq /= len(dataloader)
+
+    # return means and stds
+    return mean.cpu(), torch.sqrt(mean_sq - mean**2).cpu()
+
+
+    
 if __name__ == "__main__":
-
     # test
-    dataset = CRC_Dataset(
-        root_dir = os.path.join(os.getcwd(), 'data\\train'),
-        transforms = [MirrorPad(((92,), (92,), (0,))), ToTensor()]
+    dataset = CRC_Dataset(root_dir = os.path.join(os.getcwd(), 'data\\train'))
+
+    #print(dataset.images[:10], dataset.labels[:10], len(dataset))
+
+    #t1 = time.perf_counter()
+    #image, label = dataset[np.random.randint(len(dataset))]
+    #t2 = time.perf_counter() - t1
+
+    #print(f"{CRC_Dataset.__getitem__} took {t2}s")
+    #print(image.shape, label.shape, image.dtype, label.dtype)
+
+    #plt.imshow(image.numpy().transpose((1, 2, 0)))
+    #plt.show()
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset = dataset,
+        batch_size = 32,
+        pin_memory = True,
+        num_workers = 2
     )
 
-    print(dataset.images[:10], dataset.labels[:10], len(dataset))
+    mean, std = compute_mean_and_std(dataloader, device="cuda:0") #set to "cpu" if not gpu is available
+    print(mean, std) #prints approx. mean=(0.7979, 0.6772, 0.7768), std=(0.1997, 0.3007, 0.2039)
 
-    t1 = time.perf_counter()
-    image, label = dataset[np.random.randint(len(dataset))]
-    t2 = time.perf_counter() - t1
 
-    print(f"{CRC_Dataset.__getitem__} took {t2}s")
-    print(image.shape, label.shape, image.dtype, label.dtype)
 
-    plt.imshow(image.numpy().transpose((1, 2, 0)))
-    plt.show()
 
    
 
